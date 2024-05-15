8eb2ca5d69f38f8162a5a103187c680c80e1fc8e
V Athithya Ramaa <149147655+athithyaramaa1@users.noreply.github.com>
2024-05-15 18:13:05 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,11 @@ const router = require('./toDoRoutes');
 require('dotenv').config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 connectDB();
 app.use(router);
 const port = process.env.PORT || 5004;
